b7ed6a65474f81e6b98be5fb336199b3ae12edb2
Mark <mark@nebulagames.net>
2020-08-21 09:37:25 -0500

    Original Commit
    Base Structure Created.

---
+++ development/ACT_Solution/ACT_Core/about.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ACT.Core
+{
+    public class About
+    {
+        public override string ToString()
+        {
+            return "Written originally as DINFOPortal this has migrated for over 20 years now into a much smaller, simplified development helper.";
+        }
+    }
+}
